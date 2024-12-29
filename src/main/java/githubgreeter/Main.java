962b5056914145bd9b8e14910d8f29b06deb79a4
coderHuahua <86610939+coderHuahua@users.noreply.github.com>
2024-12-29 16:59:32 -0500

    Initial commit

---
+++ src/main/java/githubgreeter/Main.java
@@ -0,0 +1,12 @@
+package githubgreeter;
+
+public class Main{
+  public static void main(String[] args) {
+    // Instantiate a GitHubGreeter object
+  
+
+    // Call the methods you wrote and confirm the correct format
+
+
+  }
+}
